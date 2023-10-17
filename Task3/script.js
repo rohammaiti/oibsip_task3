7a26a19dfa47ad8f3d60f6ce67b5dd3245a633ee
rohammaiti <95322992+rohammaiti@users.noreply.github.com>
2023-10-17 10:07:06 +0530

    Update script.js

--- Task3/script.js
+++ Task3/script.js
@@ -11,7 +11,7 @@ function addTask() {
     }
   }
   
-  // Function to mark a task as completed and move it to the completed tasks list
+ 
   function markAsComplete() {
     const completedTasksList = document.getElementById("completed-tasks");
     completedTasksList.appendChild(this);
@@ -19,7 +19,7 @@ function addTask() {
     this.addEventListener("click", markAsPending);
   }
   
-  // Function to mark a completed task as pending and move it back to the pending tasks list
+  
   function markAsPending() {
     const pendingTasksList = document.getElementById("pending-tasks");
     pendingTasksList.appendChild(this);
@@ -27,10 +27,7 @@ function addTask() {
     this.addEventListener("click", markAsComplete);
   }
   
-  // You can also add functions for editing and deleting tasks here
-  // For editing, you can replace the task text with an input field, and for deleting, remove the task from the list.
   
-  // Example of deleting a task
   function deleteTask() {
     this.remove();
   }
